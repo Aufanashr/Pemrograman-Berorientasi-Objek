7444bf54f86306208aa01da21228cfb963eafea0
Ashh <aufaarn01@gmail.com>
2026-09-01 21:43:53 +0700

    Add files via upload

---
+++ Pertemuan-2/Tugas.java
@@ -0,0 +1,12 @@
+
+
+public class Tugas {
+    public static void main(String[] args) {
+        
+        System.out.println("Nama   : Aufa Abid Rahman");
+        System.out.println("NPM    : 2505060055");
+        System.out.println("Alamat : KENDAL");
+        System.out.println("No_HP  : 0895415452989");
+        
+    }
+}
